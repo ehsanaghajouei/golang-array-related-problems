a928da8c3aa902a11bbf8f1feee1c6392ff5ee74
ehsanaghajouei <ehsanaghajouei@gmail.com>
2024-03-01 22:57:24 +0330

    Update problem10.go

--- problem10.go
+++ problem10.go
@@ -3,15 +3,25 @@ package main
 import "fmt"
 
 func main() {
-    array1 := []int{1, 2, 3, 4, 5}
-    array2 := []int{3, 4, 5, 6, 7}
+    array1 := []int{1, 2, 3, 4, 11}
+    array2 := []int{3, 9, 5, 6, 7}
 
     finalArray := []int{}
 
     finalArray = appender(array1, finalArray)
     finalArray = appender(array2, finalArray)
 
-    fmt.Println(finalArray)
+    fmt.Println(sorter(finalArray))
+}
+
+func sorter(array []int) []int {
+    for i := 0; i < len(array)-1; i++ {
+        if array[i] > array[i+1] {
+            array[i], array[i+1] = array[i+1], array[i]
+            i = 0
+        }
+    }
+    return array
 }
 
 func appender(source []int, destination []int) []int {
